e5869149f238e2fb5722e1b2b5166137bb13b48f
devsalomao <christophersalomao5@gmail.com>
2025-07-10 16:36:09 -0300

    feat: rotas criadas em routes.tsx e implementadas em main.tsx

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,11 +1,49 @@
-import logo from "./assets/logo.png"
+import { createBrowserRouter } from "react-router-dom";
 
-function App() {
-  return (
-    <>
-      <img src={logo} alt="Logo" />
-    </>
-  )
-}
+import { Layout } from "./components/Layout";
 
-export default App;
+import Home from "./pages/Home";
+import VehicleDetail from "./pages/VehicleDetail";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+import NewVehicle from "./pages/Dashboard/NewVehicle";
+
+import { NotFound } from "./pages/NotFound";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "detalhes/:id",
+        element: <VehicleDetail />,
+      },
+      {
+        path: "login",
+        element: <Login />,
+      },
+      {
+        path: "cadastro",
+        element: <Register />,
+      },
+      {
+        path: "dashboard",
+        element: <Dashboard />,
+      },
+      {
+        path: "dashboard/novo-veiculo",
+        element: <NewVehicle />,
+      },
+      {
+        path: "*",
+        element: <NotFound />,
+      },
+    ],
+  },
+]);
