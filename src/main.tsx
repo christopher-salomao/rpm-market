deba26b435c48794afe9267231eabb53fafa7b18
Christopher Salomão <christophersalomao5@gmail.com>
2025-08-16 10:38:20 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -9,10 +9,7 @@ import { AuthProvider } from "./contexts/AuthContext/provider";
 
 import { register } from "swiper/element/bundle";
 register();
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
-import "swiper/css/scrollbar";
+import "swiper/swiper-bundle.css";
 
 import "./index.css";
 
